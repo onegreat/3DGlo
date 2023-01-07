6ba2b8dcf0063aa0312985e33cd0d78a177f392c
Onegreat <wangreat@yandex.ru>
2023-01-07 14:55:57 +0300

    ДЗ 24

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -7,11 +7,38 @@ const calc = (price = 100) => {
     const total = document.getElementById('total')
 
     const countCalc = () => {
-        console.log('calc');
+        const calcTypeValue = +calcType.options[calcType.selectedIndex].value
+        const calcSquareValue = calcSquare.value
+
+        let totalValue = 0
+        let calcCountValue = 1
+        let calcDayValue = 1
+
+        if (calcCount.value > 1) {
+            calcCountValue += +calcCount.value / 10
+        }
+
+        if (calcDay.value && calcDay.value < 5) {
+            calcDayValue = 2
+        } else if (calcDay.value && calcDay.value < 10) {
+            calcDayValue = 1.5
+        }
+
+        if (calcType.value && calcSquare.value) {
+            totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue
+        } else {
+            totalValue = 0
+        }
+
+        total.textContent = totalValue
     }
 
     calcBlock.addEventListener('input', (e) => {
-        countCalc()
+        // countCalc()
+        if (e.target === calcType || e.target === calcSquare ||
+            e.target === calcCount || e.target === calcDay) {
+            countCalc()
+        }
     })
 }
 
